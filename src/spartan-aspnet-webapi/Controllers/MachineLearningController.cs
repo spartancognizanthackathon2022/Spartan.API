900894cc4ec2f91bdaac19c22def740818a74f3d
Zuhaer Zim <zuhaer.zim@newsignature.com>
2022-05-14 19:32:27 -0400

    moved aspnet solution to new folder structure

---
+++ src/spartan-aspnet-webapi/Controllers/MachineLearningController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using spartan_claim_service.Models;
+
+namespace spartan_claim_service.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineLearningController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> PostScore(ClaimsTest claim)
+        {
+            var handler = new HttpClientHandler()
+            {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) => { return true; }
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                // Request data goes here
+                var scoreRequest = new
+                {
+                    Inputs = new Dictionary<string, List<Dictionary<string, string>>>()
+                    {
+                        {
+                            "data",
+                            new List<Dictionary<string, string>>()
+                            {
+                                new Dictionary<string, string>()
+                                {
+                                    {
+                                        "Provider", claim.AttendingPhysician.ToString()
+                                    },
+                                                                        {
+                                        "InscClaimAmtReimbursed", claim.InscClaimAmtReimbursed.ToString()
+                                    },
+                                    {
+                                        "AttendingPhysician", claim.Provider.ToString()
+                                    },
+                                    {
+                                        "is_inpatient", claim.IsInpatient ? "1" : "0"
+                                    },
+                                    {
+                                        "Gender", claim.Gender.ToString()
+                                    },
+                                    {
+                                        "Race", claim.Race.ToString()
+                                    },
+                                                                        {
+                                        "RenalDiseaseIndicator", claim.RenalDiseaseIndicator ? "1" : "0"
+                                    },
+                                    {
+                                        "State", claim.State.ToString()
+                                    },
+                                    {
+                                        "County", claim.County.ToString()
+                                    },
+                                    {
+                                        "ChronicCond_Alzheimer", claim.ChronicCondAlzheimer ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_Heartfailure", claim.ChronicCondHeartfailure ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_KidneyDisease", claim.ChronicCondKidneyDisease ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_Cancer", claim.ChronicCondCancer ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_ObstrPulmonary", claim.ChronicCondObstrPulmonary ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_Depression", claim.ChronicCondDepression ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_Diabetes", claim.ChronicCondDiabetes ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_IschemicHeart", claim.ChronicCondIschemicHeart ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_Osteoporasis", claim.ChronicCondOsteoporasis ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_rheumatoidarthritis", claim.ChronicCondRheumatoidarthritis ? "1" : "0"
+                                    },
+                                    {
+                                        "ChronicCond_stroke", claim.ChronicCondStroke ? "1" : "0"
+                                    },
+                                    {
+                                        "IPAnnualReimbursementAmt", claim.IpannualReimbursementAmt.ToString()
+                                    },
+                                    {
+                                        "IPAnnualDeductibleAmt", claim.IpannualDeductibleAmt.ToString()
+                                    },
+                                    {
+                                        "OPAnnualReimbursementAmt", claim.OpannualReimbursementAmt.ToString()
+                                    },
+                                    {
+                                        "OPAnnualDeductibleAmt", claim.OpannualDeductibleAmt.ToString()
+                                    },
+                                    {
+                                        "age", claim.Age.ToString()
+                                    },
+                                    {
+                                        "Is_Dead", claim.IsDead ? "1" : "0"
+                                    },
+                                    {
+                                        "DaysAdmitted", claim.DaysAdmitted.ToString()
+                                    },
+                                    {
+                                        "TotalDiagnosis", claim.TotalDiagnosis.ToString()
+                                    },
+                                    {
+                                        "TotalProcedure", claim.TotalProcedure.ToString()
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                        {
+                            "method", "predict"
+                        }
+                    }
+                };
+
+                const string apiKey = "5Bglxe0ccKKTdEsGDuFKuDQuB2sxLZ5S"; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("http://1317a3f1-2793-4bb0-83ce-f1dfe9e4056c.eastus.azurecontainer.io/score");
+
+                var requestString = JsonConvert.SerializeObject(scoreRequest);
+                var content = new StringContent(requestString);
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                // WARNING: The 'await' statement below can result in a deadlock
+                // if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false)
+                // so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+                HttpResponseMessage response = await client.PostAsync("", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+
+                    return Ok(new { result });
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp,
+                    // which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                    return BadRequest(new { status = "error", message = "error getting score" });
+                }
+            }
+        }
+    }
+}
---
+++ src/spartan-aspnet-webapi/Models/ClaimsTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+#nullable disable
+
+namespace spartan_claim_service.Models
+{
+    public partial class ClaimsTest
+    {
+        [JsonProperty("beneId")]
+        public string BeneId { get; set; }
+        [JsonProperty("attendingPhysician")]
+        public string AttendingPhysician { get; set; }
+        [JsonProperty("provider")]
+        public string Provider { get; set; }
+        [JsonProperty("inscClaimAmtReimbursed")]
+
+        public Int32 InscClaimAmtReimbursed { get; set; }
+        [JsonProperty("isInpatient")]
+
+        public bool IsInpatient { get; set; }
+        [JsonProperty("gender")]
+
+        public byte Gender { get; set; }
+        [JsonProperty("state")]
+
+        public byte State { get; set; }
+        [JsonProperty("county")]
+
+        public Int16 County { get; set; }
+        [JsonProperty("race")]
+
+        public byte Race { get; set; }
+        [JsonProperty("renalDiseaseIndicator")]
+
+        public bool RenalDiseaseIndicator { get; set; }
+        [JsonProperty("chronicCondAlzheimer")]
+
+        public bool ChronicCondAlzheimer { get; set; }
+        [JsonProperty("chronicCondHeartfailure")]
+
+        public bool ChronicCondHeartfailure { get; set; }
+        [JsonProperty("chronicCondKidneyDisease")]
+
+        public bool ChronicCondKidneyDisease { get; set; }
+        [JsonProperty("chronicCondCancer")]
+
+        public bool ChronicCondCancer { get; set; }
+        [JsonProperty("chronicCondObstrPulmonary")]
+
+        public bool ChronicCondObstrPulmonary { get; set; }
+        [JsonProperty("chronicCondDepression")]
+
+        public bool ChronicCondDepression { get; set; }
+        [JsonProperty("chronicCondDiabetes")]
+
+        public bool ChronicCondDiabetes { get; set; }
+        [JsonProperty("chronicCondIschemicHeart")]
+
+        public bool ChronicCondIschemicHeart { get; set; }
+        [JsonProperty("chronicCondOsteoporasis")]
+
+        public bool ChronicCondOsteoporasis { get; set; }
+        [JsonProperty("chronicCondRheumatoidarthritis")]
+        public bool ChronicCondRheumatoidarthritis { get; set; }
+        [JsonProperty("chronicCondStroke")]
+
+        public bool ChronicCondStroke { get; set; }
+        [JsonProperty("ipannualReimbursementAmt")]
+
+        public Int32 IpannualReimbursementAmt { get; set; }
+        [JsonProperty("ipannualDeductibleAmt")]
+
+        public double IpannualDeductibleAmt { get; set; }
+        [JsonProperty("opannualReimbursementAmt")]
+
+        public double OpannualReimbursementAmt { get; set; }
+        [JsonProperty("opannualDeductibleAmt")]
+
+        public double OpannualDeductibleAmt { get; set; }
+        [JsonProperty("age")]
+
+        public double Age { get; set; }
+        [JsonProperty("isDead")]
+
+        public bool IsDead { get; set; }
+        [JsonProperty("daysAdmitted")]
+
+        public Double DaysAdmitted { get; set; }
+        [JsonProperty("totalDiagnosis")]
+
+        public double TotalDiagnosis { get; set; }
+        [JsonProperty("totalProcedure")]
+
+        public double TotalProcedure { get; set; }
+        [JsonProperty("id")]
+
+        public Int32 Id { get; set; }
+    }
+}
